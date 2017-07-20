45f150098fca1d632977cdb00b6dd045a38b4d81
rladbal0101 <rladbal0101@naver.com>
2017-07-20 18:23:45 +0900

    170720 추가

--- artica/js/function.js
+++ artica/js/function.js
@@ -42,104 +42,90 @@ $(function(){
 
   });
 
-  // portfolio list
+  // about us - my story
+  $('.item-photo-mask').on('click', function(){
 
-  var prevLineNumber = -1; // 리스트 상세내용이 선택되어 보이지 않은 상태
+    $('.item-story-wrap').css({
+      height:0
+    });
 
-  $('.pofol-list-item>.thumb').on('click', function(){
+    $(this).parent().next().css({
+      height : 279,
+      transition : 'height .3s ease-out'
+    });
 
-    var indexNumber = $(this).parent().index('.pofol-list-item');
-
-    var lineNumber =  Math.floor(indexNumber/4);
-
-    var detailTop = parseInt( $('.detail-wrap').css('top')) + (lineNumber * 230 );
-
-    if(prevLineNumber == -1){
-      $(this).next().css({
-        top: detailTop,
-        height : 486,
-        transition:'height .5s ease-out'
-      });
-
-      for(var k=0; k<=3; k++){
-        $('.pofol-list-item').eq(lineNumber*4 + k).css({'padding-bottom' : 490});
-      }
-
-      prevLineNumber = lineNumber;
-
-    } else if(prevLineNumber == lineNumber){
-      $('.detail-wrap').css({
-        height:0,
-        transition:'none'
-      });
-
-      $(this).next().css({
-        top: detailTop,
-        height : 486
-      });
-
-      for(var l=0; l<=3; l++){
-        $('.pofol-list-item').eq(lineNumber*4 + l).css({'padding-bottom' : 490});
-      }
-
-      prevLineNumber = lineNumber;
-
-    } else {
-      $('.detail-wrap').css({
-        height:0,
-        transition:'height .5s ease-out'
-      });
-
-      for(var j=0; j<=3; j++){
-        $('.pofol-list-item').eq(prevLineNumber*4 + j).css({'padding-bottom' : 0});
-      }
-
-      $(this).next().css({
-        top: detailTop,
-        height : 486
-      });
-      for(var i=0; i<=3; i++){
-        $('.pofol-list-item').eq(lineNumber*4 + i).css({'padding-bottom' : 490});
-      }
-
-      prevLineNumber = lineNumber;
-
-    }
-
-    /*
-    if( indexNumber >=0 && indexNumber <=3 ){
-
-      for( var i=0; i<3; i++ ){
-        $('.pofol-list-item').eq(i).css({'padding-bottom' : 490});
-      }
-
-    } else if ( indexNumber >=4 && indexNumber <=7 ){
-
-      for( var j=4; j<7; j++ ){
-
-        $('.pofol-list-item').eq(j).css({'padding-bottom' : 490});
-      }
-
-    }
-    */
+    $('.section5').css({
+      'padding-bottom' : 279,
+      transition : 'padding-bottom .3s ease-out'
+    });
 
   });
 
-  $('.pofol-list-item .btn-pop-close').on('click', function(){
+  $('.js-item-story-close').on('click', function(){
 
-    var indexNumber = $(this).parents('.pofol-list-item').index('.pofol-list-item');
+    $(this).parents('.item-story-wrap').css({
+      height : 0,
+      transition : 'height .3s ease-out'
+    });
 
-    console.log(indexNumber);
-
-    var lineNumber =  Math.floor(indexNumber/4);
-
-    $('.detail-wrap').css({height:0});
-
-    for(var k=0; k<=3; k++){
-      $('.pofol-list-item').eq(lineNumber*4 + k).css({'padding-bottom' : 0});
-    }
+    $('.section5').css({
+      'padding-bottom' : 0,
+      transition : 'padding-bottom .3s ease-out'
+    });
 
   });
+
+  // our skills
+  // bar graph
+  $('.skill-list').on('click', function(){
+
+    var barAmount = ['p90', 'p80', 'p93', 'p85'];
+
+    $('.skill-list-item>.bar').each(function(index){
+
+      $(this).addClass(barAmount[index]);
+
+    });
+
+  });
+
+  // number count
+  $('.career-list').on('click', function(){
+
+    var count=0;
+    var countTime = setInterval(function(){
+      if(count <= 230) {
+        $('.career-number').eq(0).text(count);
+      }
+      if(count <= 22) {
+        $('.career-number').eq(1).text(count);
+      }
+      if(count <= 2380) {
+        $('.career-number').eq(2).text(count);
+      }
+      if(count <= 50890) {
+        $('.career-number').eq(3).text(count);
+      }
+      if(count > 50890){
+        clearInterval(countTime);
+      }
+
+      count++;
+    }, 10);
+
+  });
+
+  // blog image scale up
+  $('.blog-list-item').on('mouseenter', function(){
+
+  });
+
+  // inview
+  $('.section5').on('inview', function(visible){
+
+    console.log(visible);
+  });
+
 
 });
 
